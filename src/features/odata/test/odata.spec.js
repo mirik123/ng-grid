f4ae5fd515c18816cdafa6a43b7686e242ed4768
Mark Babayev <mirik123@users.noreply.github.com>
2015-08-28 13:20:18 +0300

    fixed odata.spec.js file

--- src/features/odata/test/odata.spec.js
+++ src/features/odata/test/odata.spec.js
@@ -3,35 +3,32 @@
   'use strict';
   describe('ui.grid.odata uiGridOdataService', function () {
     var uiGridOdataService;
+    var uiGridExpandableService;
     var grid;
     var gridClassFactory;
     var uiGridConstants;
     var $rootScope;
     var $scope;
+    var isExpanded;
 
+    beforeEach(module('ui.grid.expandable'));
     beforeEach(module('ui.grid.odata'));
 
-    beforeEach(inject(function (_uiGridOdataService_, _gridClassFactory_, _uiGridConstants_, _$rootScope_) {
+    beforeEach(inject(function (_uiGridExpandableService_, _uiGridOdataService_, _gridClassFactory_, _uiGridConstants_,
+                                _$rootScope_, _gridUtil_, $templateCache) {
+
+      uiGridExpandableService = _uiGridExpandableService_;
       uiGridOdataService = _uiGridOdataService_;
       gridClassFactory = _gridClassFactory_;
       uiGridConstants = _uiGridConstants_;
       $rootScope = _$rootScope_;
       $scope = $rootScope.$new();
+      isExpanded = false;
 
       grid = gridClassFactory.createGrid({});
 
       grid = {
-        enableCellEditOnFocus: true,
-        enableCellSelection: true,
-        enableSorting: true,
-        enableGridMenu: true,
-        enableColumnResizing: true,
         enableFiltering: true,
-        treeRowHeaderAlwaysVisible: true,   //???
-        //paginationPageSize: 5,
-        enablePagination: false,
-        //paginationPageSizes: [5,10,15],
-        enableRowSelection: true,
         expandableRowTemplate: 'ui-grid/odataExpandableRowTemplate',
 
         //https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/500_complex.json
@@ -48,19 +45,29 @@
 
       grid.onRegisterApi = function (gridApi) {
         gridApi.expandable.on.rowExpandedStateChanged($scope, function(row) {
-          //gridUtil.logDebug('expanded: ' + row.entity.Description);
+          it('rowExpandedStateChanged: ' + row.entity.Description);
         });
 
         gridApi.odata.on.success($scope, function(grid) {
-          //gridUtil.logDebug('succeeded');
+          it('succeeded');
+          it('expand row', function() {
+            expect(grid.rows.length).toBeGreaterThan(0);
+
+            if (!isExpanded) {
+              isExpanded = true;
+              var row = grid.rows[0];
+              grid.api.expandable.toggleRowExpansion(row.entity);
+            }
+          });
         });
 
         gridApi.odata.on.error($scope, function(data, message) {
-          //gridUtil.logError(message);
+          it('error: ' + message);
         });
       };
 
-      uiGridOdataService.initializeGrid(grid, $scope);
+      uiGridExpandableService.initializeGrid(grid);
+      uiGridOdataService.initializeGrid(grid, true);
     }));
   });
 })();
